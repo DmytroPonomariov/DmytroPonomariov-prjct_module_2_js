973ec4d56effe8eccf58fd58e3c7dba2b8cf54e7
Dmytro <dima281998@gmail.com>
2024-08-04 18:41:05 +0100

    homework1.0

--- homework 1/app.js
+++ homework 1/app.js
@@ -1,1 +1,44 @@
 // homework1
+
+"use strict";
+
+// const name = "Anna";
+
+// console.log("Hello, " + name);
+// console.log(`Hello, ${name}`);
+
+// Assignment 1
+
+const standartRate = 10
+const overtimeRate = 12
+
+console.log(standartRate + overtimeRate)
+console.log(standartRate - overtimeRate)
+console.log(standartRate * overtimeRate)
+console.log(standartRate / overtimeRate)
+console.log(standartRate ** 2)
+console.log(Math. sqrt(standartRate))
+
+
+// Assignment 2
+// Typeof написав для самоперевірки. 
+// З числа в рядок і булове
+let age = 18 
+age = "18"
+
+console.log(typeof age)
+console.log(typeof !!age)
+
+// З рядка в число і булове
+let year = "2024" 
+year = 2025
+
+console.log(typeof year)
+console.log(typeof !!year)
+
+// З буловго в число і рядок
+let isActive = true
+isActive = 3
+isActive = "3"
+
+console.log(typeof isActive)
