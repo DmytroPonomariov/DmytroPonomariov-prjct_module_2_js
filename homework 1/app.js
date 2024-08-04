263e2a8e9bcbf5a12929f029a069ac548e2876bf
Dmytro <dima281998@gmail.com>
2024-08-04 19:46:00 +0100

    homework1.1.last version

--- homework 1/app.js
+++ homework 1/app.js
@@ -42,3 +42,58 @@ isActive = 3
 isActive = "3"
 
 console.log(typeof isActive)
+
+// можливо це все що я зробив це не пертворення, а перепризначення(перепрописання)
+// А перетворення нижче 
+
+// З рядка в число і булове
+const year1 = "2023";
+
+const date = Number(year1);
+const date2 = +year1;
+const date3 = Boolean(year1)
+
+console.log(date);
+console.log(date2);
+console.log(!!date2);
+console.log(date3)
+
+
+const sum = "200euro";
+
+const parsedSum = parseFloat(sum);
+
+console.log(parsedSum);
+
+const sum1 = "0euro";
+
+const parsedSum1 = parseFloat(sum1);
+const BooleanSum2 = Boolean(sum1)
+
+console.log(parsedSum1);
+console.log(!!parsedSum1);
+console.log(typeof !!parsedSum1);
+console.log(BooleanSum2);
+console.log(typeof BooleanSum2);
+
+
+
+
+// З числа в рядок і булове 
+
+const currency = 100;
+const convertedToString = currency.toString();
+const convertedToBoolean = Boolean(currency)
+
+console.log(convertedToString);
+console.log(typeof convertedToString);
+console.log(!!convertedToString);
+console.log(typeof !!convertedToString);
+console.log(convertedToBoolean);
+console.log(typeof convertedToBoolean);
+
+const currency2 = 200; 
+const convertedToString2 = currency2 + "";
+
+console.log(convertedToString2);
+console.log(typeof convertedToString2)
