d97211d98379f6c17714ae58a0a72f71b71e9f50
Dmytro <dima281998@gmail.com>
2024-09-08 17:11:42 +0100

    homework5.1

---
+++ app.js
@@ -0,0 +1,66 @@
+// Homework 5
+
+// Assignment 1
+// Math.abs(Math.round(days))
+
+// const startDate = new Date("07/15/2015");  
+// const endDate = new Date("11/28/2016");  
+
+// function daysDurationBetweenDates(startDate, endDate, type) {
+//     switch (type) {
+//         case "days":
+//         let days = (endDate.getTime() - startDate.getTime()) / 1000;
+//         days /= (60 * 60 * 24);
+//         return days + 'days'
+//     }
+
+// }
+
+// console.log(daysDurationBetweenDates(startDate, endDate, "days"))
+
+
+// Assignment 2
+
+// const resultsArray = [1, 2, [3, [4]]];
+
+// const expression = resultsArray.flat(Infinity)
+// .reduce(function(accumulator, currentValue) {
+//     return accumulator * currentValue
+// });
+
+// console.log(expression);
+
+// приклад об'єкту
+// const priceData = {
+//     Apples: '23.4',
+//     BANANAS: '48',
+//     oRAngGEs: '48.7584',
+//     };
+
+// let entries = Object.entries(priceData);
+// let capsEntries = entries.map((entry) => [entry[0][0].toUpperCase() + entry[0].slice(1), entry[1]]);
+// let capsPriceData = Object.fromEntries(capsEntries);
+
+// console.log(capsPriceData);
+
+
+// function LowerCase {
+//     let entries = 
+// }obj =>
+//   Object.keys(obj).reduce((acc, k) => {
+//     acc[k.toLowerCase()] = obj[k];
+//     return acc;
+//   }, {});
+
+// lowerize({ Name: 'John', Age: 22 });
+
+// console.log(lowerize)
+
+
+
+    
+    // function optimizer(data) {
+    //     // тут ваш код
+    // };
+
+    new Object
