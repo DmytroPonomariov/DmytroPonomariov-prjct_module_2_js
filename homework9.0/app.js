1bc7bc425e99a4ec099baf64c371e5281a799445
DmytroPonomariov <dima281998@gmail.com>
2024-09-27 04:28:25 +0100

    homework9.1" git push origin head
    git commit -m homework9.1
    git push origin head
    
    git commit -m 'homework9.1

---
+++ homework9.0/app.js
@@ -0,0 +1,38 @@
+// let buttonOff = document.querySelector(".glow-on-hover");
+// let textButton = buttonOff.innerText === 'TURN OFF'
+
+// buttonOff.addEventListener('click', () =>{
+//     textButton !== 'TURN OFF' ? 'TURN ON' : 'TURN OFF'
+
+// });
+
+
+// function toggle() {
+//     if (document.getElementById("unit").innerHTML === "metric") {
+//         document.getElementById("unit").innerHTML = "imperial";
+//     } else  {
+//         document.getElementById("unit").innerHTML = "metric";
+//     }
+// }
+
+
+
+
+
+// function changeLabel() {
+//     const buttonOff = 
+//         document.querySelector('.glow-on-hover');
+//     buttonOff.innerHTML = 'Button Clicked!';
+// }
+
+// function toggleText(event) {
+//     const buttonOff = event.textContent || event.innerText;
+//     if (text == 'TURN OFF') {
+//         event.innerHTML = 'ON';
+//     }
+//     else {
+//         event.innerHTML = 'TURN OFF';
+//     }
+// }
+
+// eve
