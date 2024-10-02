db4d65735d3f6223d994c7a3794654f41684db8a
DmytroPonomariov <dima281998@gmail.com>
2024-10-02 15:00:45 +0100

    homework10.1

--- homework10.0/app.js
+++ homework10.0/app.js
@@ -1,4 +1,105 @@
 "use strict";
 
+// 1. Додати нові задачі
+// 2. Видаляти всі задачі
+// 3. Видаляти окремі задачі
+// 4. Пошук/фільтрація
+// 5. Зберігання в стореджі
+
+const taskInput = document.querySelector('.task-input');
+const form = document.querySelector('.create-task-form');
+const collection = document.querySelector('.collection');
+const clearButton = document.querySelector('.clear-tasks');
 
 
+
+const TASKS_STORAGE_KEY = 'tasks';
+
+form.addEventListener('submit', addTask)
+document.addEventListener('DOMContentLoaded', renderTasks);
+clearButton.addEventListener('click', removeTasks);
+collection.addEventListener('click', removeTask)
+
+function renderTasks() {
+    if (getTasksFromLocalStorage()) {
+        const tasks = JSON.parse(getTasksFromLocalStorage())
+
+        tasks.forEach((task) => { 
+            createTask(task)
+        })
+    }
+}
+
+function createTask(task) {
+    const li = document.createElement('li');
+    li.innerHTML = task;
+    li.classList.add('task')
+
+    const button = document.createElement('button');
+    button.innerHTML = 'X';
+    button.className = 'button-icon button-delete'
+    li.append(button)
+
+
+
+    collection.append(li);
+}
+
+function addTask(event) {
+    event.preventDefault();
+
+    const currentForm = event.target;
+    const inputValue = currentForm.task.value;
+
+    if (!inputValue){
+        return;
+    }
+
+    createTask(inputValue);
+    setTaskToLocalStorage(inputValue);
+
+    currentForm.reset();
+}
+
+
+
+function setTaskToLocalStorage(task) {
+
+    let tasks = [];
+
+    if(getTasksFromLocalStorage()) {
+        tasks = JSON.parse(getTasksFromLocalStorage())
+    }
+
+    tasks.push(task)
+
+    setTasksToLocalStorage(tasks)
+}
+
+function getTasksFromLocalStorage() {
+    return localStorage.getItem(TASKS_STORAGE_KEY)
+}
+
+function setTasksToLocalStorage(tasks) {
+    return localStorage.setItem(TASKS_STORAGE_KEY, JSON.stringify(tasks))
+
+}
+
+function clearTasksFromLocalStorage() {
+    localStorage.removeItem(TASKS_STORAGE_KEY);
+}
+
+function removeTasks() {
+    collection.innerHTML = '';
+    clearTasksFromLocalStorage();
+}
+
+function removeTask(event) {
+    if (event.target.classList.contains('button-delete')) {
+        const li = event.target.closest('.task')
+        console.log(li)
+        console.log(event.target)
+        li.remove();
+    }
+
+}
