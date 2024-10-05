eacf3303be600c06b343b440a2acbfcb32ec4beb
DmytroPonomariov <dima281998@gmail.com>
2024-10-05 14:33:29 +0100

    homework8.1

---
+++ homework8.0/app.js
@@ -0,0 +1,23 @@
+let h2WithId = document.getElementById('headerTwo');
+
+console.log(h2WithId)
+
+
+const firstSection = document.querySelector('.firstSection');
+const firstSectionElement = firstSection.firstElementChild;
+console.log(firstSectionElement);
+
+const ulWithTag = document.querySelector('ul');
+const lastLi = ulWithTag.lastElementChild;
+const liNumber5 = lastLi.previousElementSibling
+console.log(liNumber5)
+
+const divWithClass = document.getElementsByClassName('hatredLevelBlock')
+console.log(divWithClass)
+
+
+
+
+
+
+
