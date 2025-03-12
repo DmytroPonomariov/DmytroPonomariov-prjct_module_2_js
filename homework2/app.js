c842c4bc724e6a72fab61fa105a217f0e14d993c
DmytroPonomariov <dima281998@gmail.com>
2025-03-12 17:16:06 +0000

    home2.5

--- homework2/app.js
+++ homework2/app.js
@@ -4,52 +4,159 @@
 
 // Assignment 1
 
+// let i = 1;
 
-for (let number = 1; number < 101; number = number + 1) {
-    if (number % 3 === 0 && number % 5 === 0) {
-        console.log("lolkek")
-        continue;
+// while(i < 1){
+//     console.log(i);
+//     i++;
+// }
+
+// let p = 1;
+
+// do {
+//     console.log(p);
+//     p++;
+// } while(p < 1);
+
+
+// let i = 0;
+
+// while(i < 3) {
+//     console.log(i);
+//     i++;
+// }
+
+// let i = 0;
+
+// do {
+//     console.log(i);
+//     i++;
+// } while(i > 3);
+
+// for (let i = 0; i < 101; i++) {
+//     if (i % 3 === 0 && i % 6 === 0) {
+//         console.log('shmek')
+//         continue;
+//     } else if (i % 3 === 0) {
+//         console.log('sh')
+//         continue;
+//     } else if (i % 6 === 0) {
+//         console.log('mek')
+//         continue;
+//     } else {
+//         console.log(i);
+//     }
+// }
+
+// for (let value = 1; value < 10; value++) {
+//     if (typeof value !== "number") {
+//         console.log("Таке відчуття що щось не так")
+//         break;
+//     } 
+//     if (value % 2 === 0) {
+//         console.log(value)
+//     }
+// }
+
+
+let random = 1;
+
+while (random < 10) {
+    if (typeof random !== "number") {
+        console.log("You have a problem");
+        break;
+    } else if (random % 2 === 0) {
+        console.log(random);
     }
-    if (number % 3 === 0) {
-        console.log("lol")
-        continue;
-    }
-    if (number % 5 === 0) {
-        console.log("kek")
-        continue;
-    }
-    console.log(number);
+    random++;
 }
+
+// let value = 0;
+
+// while (value < 10) {
+//   if (typeof value !== "number") {
+//     console.log("Таке чуство шо Бог десь наказує нас за шось");
+//     break;
+//   }
+
+//   if (value % 2 === 0) {
+//     console.log(value);
+//     }
+
+//   value = value + 1;
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// for (let number = 1; number < 101; number = number + 1) {
+//     if (number % 3 === 0 && number % 5 === 0) {
+//         console.log("lolkek")
+//         continue;
+//     }
+//     if (number % 3 === 0) {
+//         console.log("lol")
+//         continue;
+//     }
+//     if (number % 5 === 0) {
+//         console.log("kek")
+//         continue;
+//     }
+//     console.log(number);
+// }
 
 // Assignment 2
 
 
-let value = 0;
+// let value = 0;
 
-while (value < 10) {
-  if (typeof value !== "number") {
-    console.log("Таке чуство шо Бог десь наказує нас за шось");
-    break;
-  }
+// while (value < 10) {
+//   if (typeof value !== "number") {
+//     console.log("Таке чуство шо Бог десь наказує нас за шось");
+//     break;
+//   }
 
-  if (value % 2 === 0) {
-    console.log(value);
-  }
+//   if (value % 2 === 0) {
+//     console.log(value);
+//     }
 
-  value = value + 1;
-}
+//   value = value + 1;
+// }
 
 
-for (let value2 = 0; value2 < 10; value2 = value2 + 1) {
-    if (typeof value2 !== "number") {
-        console.log('Таке чуство шо Бог десь наказує нас за шось')
-        continue;
-    }
-    if (value2 % 2 !== 0) {
-        continue;
-    }
-    console.log(value2);
-}
+// for (let value2 = 0; value2 < 10; value2 = value2 + 1) {
+//     if (typeof value2 !== "number") {
+//         console.log('Таке чуство шо Бог десь наказує нас за шось')
+//         break;
+//     }
+//     if (value2 % 2 !== 0) {
+//         continue;
+//     }
+//     console.log(value2);
+// }
 
 
 
